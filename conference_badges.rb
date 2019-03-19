505b900ffa3a7ec88b18c1890a700049c92679e6
Jack Robbins <jackrobbins1@gmail.com>
2019-03-19 20:26:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 
 def assign_rooms(arr)
   newArr = []
-  arr.each_with_index do |el, index|
+  arr.each_with_index do |el, index+1|
     newArr << "Hello, #{el}! You'll be assigned to room #{index}!"
   end
   return newArr
